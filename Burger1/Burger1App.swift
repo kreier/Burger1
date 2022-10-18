5617c591f7708a730c7037afd26a883ddfe3bc32
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2022-10-18 12:36:35 +0700

    Initial Commit

---
+++ Burger1/Burger1App.swift
@@ -0,0 +1,17 @@
+//
+//  Burger1App.swift
+//  Burger1
+//
+//  Created by Matthias Kreier on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Burger1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Burger1UITests/Burger1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Burger1UITestsLaunchTests.swift
+//  Burger1UITests
+//
+//  Created by Matthias Kreier on 10/18/22.
+//
+
+import XCTest
+
+final class Burger1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
